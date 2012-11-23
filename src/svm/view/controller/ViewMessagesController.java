315c263943fcab5d44462784b7d1833387a8551c
patrickjost <patrick.jost@students.fhv.at>
2012-11-24 00:56:58 +0100

    ..aktualisierte files
    Signed-off-by: patrickjost <patrick.jost@students.fhv.at>

---
+++ src/svm/view/controller/ViewMessagesController.java
@@ -0,0 +1,189 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package svm.view.controller;
+
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+import svm.logic.abstraction.exception.IllegalGetInstanceException;
+import svm.logic.abstraction.exception.NotAllowException;
+import svm.logic.abstraction.jmsobjects.IMemberMessage;
+import svm.logic.abstraction.jmsobjects.ISubTeamMessage;
+import svm.logic.abstraction.transferobjects.ITransferMember;
+import svm.logic.abstraction.transferobjects.ITransferTeam;
+import svm.persistence.abstraction.exceptions.ExistingTransactionException;
+import svm.persistence.abstraction.exceptions.NoSessionFoundException;
+import svm.persistence.abstraction.exceptions.NoTransactionException;
+import svm.persistence.abstraction.exceptions.NotSupportedException;
+import svm.rmi.abstraction.controller.IRMIContestController;
+import svm.rmi.abstraction.controller.IRMISearchController;
+import svm.rmi.abstraction.factory.IRMIControllerFactory;
+import svm.view.forms.PanelMessages;
+
+/**
+ *
+ * @author Patrick
+ */
+public class ViewMessagesController {
+
+
+    private IRMIControllerFactory factory = ApplicationController.factory;
+    
+    private IRMIContestController contestController;
+    private IRMISearchController searchController;
+    private PanelMessages panelMessages;
+    private DefaultListModel listboxActiveRoles = new DefaultListModel();
+    private DefaultComboBoxModel<ITransferTeam> cmbSelectTeam = new DefaultComboBoxModel();
+    private DefaultListModel<ITransferMember> listboxNewMembersToAssign = new DefaultListModel();
+    private DefaultListModel<String> listboxLog = new DefaultListModel();
+    private DefaultListModel<ITransferMember> listboxMembersOfSelectedTeam = new DefaultListModel();
+    private DefaultListModel<ITransferTeam> allTeamsInSport = new DefaultListModel<>();
+     
+     public ViewMessagesController(PanelMessages panelMessages) {
+        try {
+            this.panelMessages = panelMessages;
+            this.searchController = factory.getRMISearchController(ApplicationController.user);
+        } catch (RemoteException ex) {
+            Logger.getLogger(ViewMemberController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void acceptSelectedContests() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void denySelectedContests() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void saveTeamMembers() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void showTeamMembers() {
+        
+        
+        
+    }
+
+    public void showMembersToAssign(ITransferMember imm) {
+        
+        
+        this.listboxNewMembersToAssign.addElement(imm);
+        this.panelMessages.getListboxNewMembersToAssign().setModel(listboxNewMembersToAssign); 
+    }
+    
+    /**
+     * Show all teams
+     *
+     *
+     */
+    public void showAllTeams() {
+        this.panelMessages.getCmbSelectTeam().setModel(cmbSelectTeam);
+
+        if (this.panelMessages.getCmbSelectTeam().getModel().getSize() == 0) {
+            try {
+
+                searchController.start();
+                try {
+                    for (ITransferTeam team : searchController.getTeams()) {
+                        cmbSelectTeam.addElement(team);
+                    }
+                } catch (IllegalGetInstanceException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (NoSessionFoundException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (RemoteException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (NotAllowException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                try {
+                    searchController.commit();
+                } catch (ExistingTransactionException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (NoSessionFoundException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (NoTransactionException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (RemoteException ex) {
+                    Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            } catch (NoSessionFoundException ex) {
+                Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalGetInstanceException ex) {
+                Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (RemoteException ex) {
+                Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NotSupportedException ex) {
+                Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+
+        }
+
+
+    }
+
+    void addMemberMsg(IMemberMessage imm) {
+        try {
+            searchController.start();
+            ITransferMember member = searchController.getMemberByUID(imm.getMember());
+            searchController.commit();
+           this.listboxLog.addElement("MemberMessage: " + imm.getType().toString() + " " + member.getFirstName() + " " + member.getLastName());       
+            this.panelMessages.getListboxLog().setModel(this.listboxLog);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NotSupportedException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExistingTransactionException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoTransactionException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RemoteException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSessionFoundException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalGetInstanceException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    void addSubTeamMsg(ISubTeamMessage tm) {
+        try {        
+            searchController.start();
+            ITransferMember member = searchController.getMemberByUID(tm.getMember());
+            searchController.commit();
+        this.listboxLog.addElement("SubTeamMessage: " + tm.getType().toString() + " " + member.getFirstName() + " " + member.getLastName());
+        this.panelMessages.getListboxLog().setModel(this.listboxLog);
+        } catch (ExistingTransactionException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoTransactionException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NotSupportedException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RemoteException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSessionFoundException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalGetInstanceException ex) {
+            Logger.getLogger(ViewMessagesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
